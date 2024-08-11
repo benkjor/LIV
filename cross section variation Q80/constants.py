5d2bde0e8e80c02b0616b99ed320cac1ca78fb34
ahmed_abokalil <ahmedabokhalil17@gmail.com>
2024-08-11 05:23:24 +0200

    final

--- cross section variation Q80/constants.py
+++ cross section variation Q80/constants.py
@@ -1,10 +1,9 @@
 # Constants
-from scipy.constants import physical_constants as pc
 s = (13e3)**2 # Center-of-mass energy squared in GeV^2
 Nc = 3  # Number of colors in QCD
 m_Z = 91.1876  # Mass of the Z boson in GeV/c^2
 Gamma_Z = 2.4952  # Decay width of the Z boson in GeV
 alpha = 1 / 137  # Fine-structure constant
-sin2th_w = pc['weak mixing angle'][0]  # Mixing angle
-e = 0.303 # Elementry charge in natural units
+sin2th_w = 0.23121  # sin^2(theta_W)
+e = 0.3028 # Elementry charge in natural units
 
