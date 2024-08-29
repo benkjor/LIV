210f9778fbb4851e4324799a9f31db3497c5f082
ahmed_abokalil <ahmedabokhalil17@gmail.com>
2024-08-29 11:22:11 +0300

    gg

--- cross section variation Q80/constants.py
+++
@@ -1,9 +0,0 @@
-# Constants
-s = (13e3)**2 # Center-of-mass energy squared in GeV^2
-Nc = 3  # Number of colors in QCD
-m_Z = 91.1876  # Mass of the Z boson in GeV/c^2
-Gamma_Z = 2.4952  # Decay width of the Z boson in GeV
-alpha = 1/137
-sin2th_w = 0.23121  # sin^2(theta_W)
-e = 0.3028 # Elementry charge in natural units
-
